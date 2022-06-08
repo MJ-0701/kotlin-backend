66b63c24d6c77267a55c6986f30174f4044ab602
MJ-0701 <jack2718@naver.com>
2022-06-08 14:50:50 +0900

    feat(valid): Custom annotation with Valid

--- src/main/kotlin/com/example/kotlinbackend/controller/delete/DeleteApiController.kt
+++ src/main/kotlin/com/example/kotlinbackend/controller/delete/DeleteApiController.kt
@@ -1,18 +1,27 @@
 package com.example.kotlinbackend.controller.delete
 
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.constraints.Min
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Size
 
 @RestController
 @RequestMapping("/api")
+@Validated // 유효성을 검증하기 위해서는 bean으로 생성 되어야 하지만 밑에 파라미터 들은 빈이 아닌 변수이기 때문에 해당 @Validated 어노테이션을 통해 하위에 있는
+// Validation annotation 들을 동작하게 해준다. -> 하위 페이지 전체를 검증
 class DeleteApiController {
 
     @DeleteMapping("/delete-mapping")
     fun deleteMapping(
         @RequestParam(value = "name") name : String,
+
+        @NotNull(message = "age 값은 필수값 입니다.")
+        @Min(value = 20, message = "20이상이어야 합니다.")
         @RequestParam(value = "age") age : Int
     ) : String{
 
@@ -24,7 +33,12 @@ class DeleteApiController {
 
     @DeleteMapping(path = ["/delete-mapping/name/{name}/age/{age}"])
     fun deleteMappingPath(
+        @Size(min = 2, max = 5, message = "name의 길이는 2~5 사이여야 합니다.")
+        @NotNull
         @PathVariable(name = "name") _name: String,
+
+        @NotNull(message = "age 값은 필수값 입니다.")
+        @Min(value = 20, message = "20이상이어야 합니다.")
         @PathVariable(name = "age") _age: Int
     ) : String{
 
--- src/main/kotlin/com/example/kotlinbackend/controller/put/PutApiController.kt
+++ src/main/kotlin/com/example/kotlinbackend/controller/put/PutApiController.kt
@@ -3,11 +3,15 @@ package com.example.kotlinbackend.controller.put
 import com.example.kotlinbackend.model.http.Result
 import com.example.kotlinbackend.model.http.UserRequest
 import com.example.kotlinbackend.model.http.UserResponse
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestMethod
 import org.springframework.web.bind.annotation.RestController
+import javax.validation.Valid
 
 @RestController
 @RequestMapping("/api")
@@ -25,7 +29,20 @@ class PutApiController {
     }
 
     @PutMapping(path = ["/put-mapping/object"]) // path = [] 이렇게 지정하면 여러 주소를 사용 할 수 있다.
-    fun putMappingObject(@RequestBody userRequest: UserRequest) : UserResponse{
+    fun putMappingObject(@RequestBody @Valid userRequest: UserRequest, bindingResult: BindingResult) : Any { // @Valid 해당 빈에 대해서만 검증
+
+        // bindingResult 를 통해 바로 exeception 을 던지지 않고 에러 내용을 담아서 따로 분기를 해줄 수 있다.
+        if(bindingResult.hasErrors()){
+         // 500 error
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.field + " : " + message + "\n")
+            }
+            return ResponseEntity.badRequest().body(msg.toString())
+        }
+
 
         // 0. Response
        return  UserResponse().apply { // todo :: 코틀린 apply 패턴 알아볼것.
